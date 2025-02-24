00fce0a035b9616aacc7d1b9396a83a2edd4125f
NasaHack <proah.nasahack@gmail.com>
2025-02-24 12:11:49 +0000

    chore(release): bump version to 1.1.5

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
 export { default } from "./lib/Proah";
+
+import Proah from "./lib/Proah";
+
+if (typeof module !== "undefined" && typeof module.exports !== "undefined") {
+  module.exports = Proah;
+  module.exports.default = Proah;
+}
