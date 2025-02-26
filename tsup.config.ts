dd5ef6211b56a9201dedd1f42464078495cda3d7
NasaHack <proah.nasahack@gmail.com>
2025-02-26 15:36:11 +0000

    chore: update tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,16 +1,28 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entry: ["src/**/*.ts"],
-  format: ["cjs", "esm"],
-  dts: true,
-  minify: true,
-  clean: true,
-  outDir: "dist",
-  splitting: false,
-  keepNames: false,
-  esbuildOptions(options) {
-    options.mangleProps = /^_/;
-    options.mangleQuoted = true;
+export default defineConfig([
+  {
+    entry: ["src/**/*.ts"],
+    format: ["esm"],
+    outExtension: () => ({ js: ".mjs" }),
+    dts: true,
+    minify: true,
+    clean: true,
+    esbuildOptions(options) {
+      options.mangleProps = /^_/;
+      options.mangleQuoted = true;
+    },
   },
-});
+  {
+    entry: ["src/**/*.cts"],
+    format: ["cjs"],
+    outExtension: () => ({ js: ".cjs" }),
+    dts: false,
+    minify: true,
+    clean: false,
+    esbuildOptions(options) {
+      options.mangleProps = /^_/;
+      options.mangleQuoted = true;
+    },
+  },
+]);
