6dc014e08cde9d8be83fb5b61ce06d029608b665
NasaHack <proah.nasahack@gmail.com>
2025-02-26 15:38:11 +0000

    refactor(src/index.ts): remove CommonJS module export from src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
-export { default } from "./lib/Proah";
-
 import Proah from "./lib/Proah";
 
-if (typeof module !== "undefined" && typeof module.exports !== "undefined") {
-  module.exports = Proah;
-  module.exports.default = Proah;
-}
+export default Proah;
